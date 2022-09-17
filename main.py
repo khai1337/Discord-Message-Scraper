fc5cb292d661faf9c42390278dad4fc395ecb183
khai <112670162+khai2rich@users.noreply.github.com>
2022-09-17 18:06:34 +0100

    star and follow kek
    star and follow kek

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+import json
+import os
+import discord
+import colorama
+
+from colorama import Fore
+from discord.ext import commands
+
+os.system('title message logger by kh#1337')
+
+channelid = input(f'{Fore.MAGENTA} > {Fore.RESET} Channel ID: ')
+
+def retrieve_messages(channelid):
+    token = input(f'{Fore.MAGENTA} > {Fore.RESET} Token: ')
+    headers = {
+        'authorization': (f'{token}')
+    }
+
+    r = requests.get(f'https://discord.com/api/v9/channels/{channelid}/messages', headers=headers)
+    jsonn = json.loads(r.text)
+    
+    for value in jsonn:
+        print(f'{Fore.MAGENTA} > {Fore.RESET} Message: ', value['content'], '\n' f'     {Fore.MAGENTA} > {Fore.RESET} Timestamp: ', '-', value['timestamp'], '\n' f'     {Fore.MAGENTA} > {Fore.RESET} Channel ID: ', '-', value['channel_id'], '\n' f'     {Fore.MAGENTA} > {Fore.RESET} Message ID: ', '-', value['id'], '\n' f'     {Fore.MAGENTA} > {Fore.RESET} Author: ', '-', value['author']['username'], value['author']['discriminator'],'\n')
+        with open('messages.txt', 'a', encoding="utf-8") as file:
+            file.write(f'> Message: ' + value['content'] + '\n' + f'     > Timestamp: ' + value['timestamp'] + '\n' f'     > Channel ID: ' + value['channel_id'] + '\n' + f'     > Message ID: ' + value['id'] + '\n' +  f'     > Author: ' + value['author']['username'] + value['author']['discriminator'] + '\n')
+    
+
+retrieve_messages(channelid)
+input(f'{Fore.MAGENTA} > {Fore.RESET} All messages succesfully logged.')
